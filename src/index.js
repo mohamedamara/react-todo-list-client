8717a318deb78299406adca35af2230799524960
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2021-06-03 23:45:36 +0100

    feat: add service worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "fontsource-roboto";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   // <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.render(
   // </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
